0a2fe9dfa5a8c52defb4cd71df84e48db4d79d6e
Kevin Chi <kevinchi118@gmail.com>
2018-06-24 03:15:42 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
     attr_accessor :kind
 
     def initialize(length, length2, length3)
-        if length == length2 == length3
+        if length == length2 && length == length3
             @kind = "equilateral"
         elsif length == length2 || length == length3 || length2 == length3
             @kind = "isosceles"
